fb45262070e741d6bc94a16b829fe4894adb143d
André Machado <csantos.machado@gmail.com>
2019-08-21 22:02:34 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -11,15 +11,12 @@ class TestSubmission(unittest.TestCase):
         return True
         
     def test_cancelUrl(self):
-            self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
+    self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.') 
+def test_successUrl(self):
+    self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.') 
+def test_redirect_to_checkout(self):
+    self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!') 
 
-        def test_successUrl(self):
-            self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
-
-        def test_redirect_to_checkout(self):
-            self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
-
-        
             
 if __name__ == '__main__':
     unittest.main()
