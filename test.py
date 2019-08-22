f148fed27fb34bcd48510029cb2e93940dc7d83f
André Machado <csantos.machado@gmail.com>
2019-08-22 19:10:38 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -1,6 +1,33 @@
 import unittest
 import re
-   
+
+
+class TestStripe(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestStripe, self).__init__(*args, **kwargs)
+        with open('order.html', 'r') as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
+    # Check if redirectToCheckout function call is present
+    def test_redirect_to_checkout(self):
+        self.assertNotEqual(self.dom_str, '.redirectToCheckout',
+                            'No stripe redirect call found!')
+
+    # Check if successUrl redirects to order_success.html
+    def test_successUrl(self):
+        self.assertRegex(self.dom_str,
+                         r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'',
+                         'No order_success.html redirect found on checkout success.')
+
+    # Check if cancelUrl redirects to order.html
+    def test_cancelUrl(self):
+        self.assertRegex(self.dom_str,
+                         r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'',
+                         'No order.html redirect found on checkout cancel.')
+
+
+ 
+
 class TestSubmission(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestSubmission, self).__init__(*args, **kwargs)
@@ -17,72 +44,6 @@ class TestSubmission(unittest.TestCase):
     def test_redirect_to_checkout(self):
         self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
     
-            
-if __name__ == '__main__':
-    unittest.main()
-import unittest
-import re
-   
-class TestSubmission(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestSubmission, self).__init__(*args, **kwargs)
-        with open('order.html', 'r') as file_descriptor:
-            self.dom_str = file_descriptor.read()
-
-    def test_1(self):
-        return True
         
-    def test_cancelUrl(self):
-        self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
-    def test_successUrl(self):
-        self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
-    def test_redirect_to_checkout(self):
-        self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
-    
-            
-if __name__ == '__main__':
-    unittest.main()
-import unittest
-import re
-   
-class TestSubmission(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestSubmission, self).__init__(*args, **kwargs)
-        with open('order.html', 'r') as file_descriptor:
-            self.dom_str = file_descriptor.read()
-
-    def test_1(self):
-        return True
-        
-    def test_cancelUrl(self):
-        self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
-    def test_successUrl(self):
-        self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
-    def test_redirect_to_checkout(self):
-        self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
-    
-            
-if __name__ == '__main__':
-    unittest.main()
-import unittest
-import re
-   
-class TestSubmission(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestSubmission, self).__init__(*args, **kwargs)
-        with open('order.html', 'r') as file_descriptor:
-            self.dom_str = file_descriptor.read()
-
-    def test_1(self):
-        return True
-        
-    def test_cancelUrl(self):
-        self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
-    def test_successUrl(self):
-        self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
-    def test_redirect_to_checkout(self):
-        self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
-    
-            
 if __name__ == '__main__':
     unittest.main()
